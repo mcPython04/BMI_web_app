3daf89d007aaba5bfe8e904ff654af85c58bf784
mcPython04 <chen1911893611@gmail.com>
2022-03-29 11:26:39 -0500

    added html form

--- web/views.py
+++ web/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index. ")
+def home(request):
+    return render(request, 'web/home.html')
 
 
 def result(request):
-    return HttpResponse("This is the result page. ")
+    if request.method == 'POST':
+        feet = request.POST["feet"]
+        inch = request.POST["inches"]
+
