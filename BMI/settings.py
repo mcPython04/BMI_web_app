37569bb5c3024932f2c72c196bcdaace1896f863
mcPython04 <chen1911893611@gmail.com>
2022-04-07 16:09:30 -0500

    added automated code coverage tool for tests

--- BMI/settings.py
+++ BMI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_nose',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Django-Nose code coverage tool
+# https://django-testing-docs.readthedocs.io/en/latest/coverage.html
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+NOSE_ARGS = [
+    '--with-coverage',
+]
