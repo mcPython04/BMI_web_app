a3ac43d1476464429c13d21dc7863e05a6825a54
mcPython04 <chen1911893611@gmail.com>
2022-04-07 08:13:42 -0500

    added several tests

---
+++ web/tests/tests_url.py
@@ -0,0 +1,30 @@
+from django.test import TestCase, Client
+from django.urls import reverse, resolve
+from web.views import *
+
+
+class TestUrls(TestCase):
+
+    def test_home_url(self):
+        path = reverse('home')
+        assert resolve(path).view_name == 'home'
+
+    def test_result_url(self):
+        path = reverse('result')
+        assert resolve(path).view_name == 'result'
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+    def test_home_view_get(self):
+        req = self.client.get(reverse('home'))
+
+        assert req.status_code == 200
+
+    def test_home_view_post(self):
+        req = self.client.post(reverse('home'))
+
+        assert req.status_code == 200
