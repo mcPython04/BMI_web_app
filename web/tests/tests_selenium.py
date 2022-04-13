ff5aea3ee8953c750a496d62186dbbcf5ee86661
mcPython04 <chen1911893611@gmail.com>
2022-04-12 20:58:34 -0500

    edited code

--- web/tests/tests_selenium.py
+++ web/tests/tests_selenium.py
@@ -2,4 +2,4 @@ from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://35.236.203.202:8000/")
+driver.get("http://127.0.0.1:8000/")
